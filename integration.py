4d692df19e9542507302ba3cc7b802333e738923
Andrews Medina <andrewsmedina@gmail.com>
2013-07-02 14:40:13 -0300

    added initial tests structure.

---
+++ integration.py
@@ -0,0 +1,21 @@
+def create_app():
+    pass
+
+
+def remove_app():
+    pass
+
+
+def deploy():
+    pass
+
+
+def verify():
+    pass
+
+
+def main():
+    create_app()
+    deploy()
+    verify()
+    remove_app()
