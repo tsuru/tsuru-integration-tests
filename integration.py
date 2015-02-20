099178846a645e2e200a295ddb7e6a8707b07fb8
Andrews Medina <andrewsmedina@gmail.com>
2015-02-20 18:39:32 -0200

    using tsuru client instead api.

--- integration.py
+++ integration.py
@@ -14,42 +14,29 @@ TEST_REPOSITORY = "git://github.com/flaviamissi/tsuru-app-sample.git"
 
 
 def create_app(token):
-    url = "{0}/apps".format(TSURU_URL)
-    data = {"name": APP_NAME, "platform": "static"}
-    r = auth_request(requests.post, url, token, data=json.dumps(data))
-    if r.status_code != 200:
-        print("Caugth error while creating app: {0} - {1}".format(r.status_code, r.text))
-        return 1
-    print("app created")
-    return r.json()["repository_url"]
+    return subprocess.call(["tsuru", "app-create", APP_NAME, "static"])
 
 
 def remove_app(token):
-    url = "{0}/apps/{1}".format(TSURU_URL, APP_NAME)
-    r = auth_request(requests.delete, url, token)
-    if r.status_code != 200:
-        return 1
-    print("remove app: {0}".format(r.text))
-    return 0
+    cmd = subprocess.Popen(["tsuru", "app-remove", "-a", APP_NAME], stdin=subprocess.PIPE)
+    return cmd.communicate("y")
 
 
 def _clone_repository(repository, dst):
     return subprocess.call(["git", "clone", repository, dst])
 
 
-def _push_repository(remote, git_dir):
-    return subprocess.call(["git", "--git-dir={0}".format(git_dir), "push", remote, "master"])
-
-
 def deploy(remote):
+    app_dir = "/tmp/test_app"
     exits = []
-    exits.append(_clone_repository(TEST_REPOSITORY, "/tmp/test_app"))
-    exits.append(_push_repository(remote, "/tmp/test_app/.git"))
-    exits.append(subprocess.call(["sudo", "rm", "-r", "/tmp/test_app"]))
+    exits.append(_clone_repository(TEST_REPOSITORY, app_dir))
+    exits.append(subprocess.call(["tsuru", "app-deploy", "-a", APP_NAME, app_dir]))
+    exits.append(subprocess.call(["sudo", "rm", "-r", app_dir]))
     if 1 in exits:
         print("deploy finished with error")
         return 1
     return 0
+
 
 def verify():
     url = "http://{0}.{1}".format(APP_NAME, TSURU_HOST)
@@ -61,75 +48,49 @@ def verify():
 
 
 def create_user():
-    url = "{0}/users".format(TSURU_URL)
-    data = {"email": USER, "password": PASSWORD}
-    r = requests.post(url, json.dumps(data)).text
-    print("create user output: {0}".format(r))
+    cmd = subprocess.Popen(["tsuru", "user-create", USER], stdin=subprocess.PIPE)
+    return cmd.communicate(PASSWORD + "\n" + PASSWORD)
 
 
 def remove_user(token):
-    url = "{0}/users".format(TSURU_URL)
-    print("remove user output: {0}".format(auth_request(requests.delete, url, token)))
+    cmd = subprocess.Popen(["tsuru", "user-remove"], stdin=subprocess.PIPE)
+    return cmd.communicate("y")
 
 
 def add_key(token):
-    url = "{0}/users/keys".format(TSURU_URL)
-    try:
-        f = open(os.path.expanduser("~/.ssh/id_rsa.pub"))
-        key = f.read()
-        f.close
-        code = auth_request(requests.post, url, token, data=json.dumps({"key": key})).status_code
-        print("key added")
-        if code != 200:
-            return 1
-        return 0
-    except IOError:
-        print("id_rsa.pub not found, create and run the tests again")
-        return 1
+    key_path = os.path.expanduser("~/.ssh/id_rsa.pub")
+    return subprocess.call(["tsuru", "key-add", "{}-key".format(USER), key_path])
 
 
 def remove_key(token):
-    url = "{0}/users/keys".format(TSURU_URL)
-    f = open(os.path.expanduser("~/.ssh/id_rsa.pub"))
-    key = f.read()
-    f.close
-    r = auth_request(requests.delete, url, token, data=json.dumps({"key": key}))
-    if r.status_code != 200:
-        return 1
-    print("remove key: {0}".format(r.text))
-    return 0
+    cmd = subprocess.Popen(["tsuru", "key-remove", "{}-key".format(USER)], stdin=subprocess.PIPE)
+    return cmd.communicate("y")
 
 
 def add_team(token):
-    url = "{0}/teams".format(TSURU_URL)
-    r = auth_request(requests.post, url, token, data=json.dumps({"name": "testteam"}))
-    if r.status_code != 200:
-        return 1
-    print("add team: {0}".format(r.text))
-    return 0
+    return subprocess.call(["tsuru", "team-create", "testteam"])
 
 
 def remove_team(token):
-    url = "{0}/teams/testteam".format(TSURU_URL)
-    r = auth_request(requests.delete, url, token)
-    if r.status_code != 200:
-        print ("error removing team: {0} - {1}".format(r.status_code, r.text))
-        return 1
-    print("success removing team: {0} - {1}".format(r.status_code, r.text))
-    return 0
+    retry = True
+    while retry:
+        cmd = subprocess.Popen(
+            ["tsuru", "team-remove", "testteam"],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        output = cmd.communicate("y")[1]
+
+        if not "it have access to apps" in output:
+            return output
 
 
 def login():
-    url = "{0}/users/{1}/tokens".format(TSURU_URL, USER)
-    data = {"password": PASSWORD}
-    r = requests.post(url, json.dumps(data))
-    if r.status_code > 201:
-        return 1
-    token = json.loads(r.text)["token"]
-    return token
-
-def auth_request(method, url, token, **kwargs):
-    return method(url, headers={"Authorization": "bearer {0}".format(token)}, **kwargs)
+    cmd = subprocess.Popen(["tsuru", "login", USER], stdin=subprocess.PIPE)
+    cmd.communicate(PASSWORD)
+    token = subprocess.check_output(["tsuru", "token-show"])
+    return token.split(" ")[-1]
 
 
 def main():
